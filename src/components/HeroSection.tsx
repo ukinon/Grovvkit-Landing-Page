81fc5cd8639a3b7b0eedb6ab2a5572005aa4c061
Arfiano Jordhy <arfianoj@gmail.com>
2024-02-13 05:24:08 +0700

    finished hero section + add about section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const texts = ["POTENTIAL", "TITIT", "MAMAK"];
+
+export default function HeroSection() {
+  const [text, setText] = useState("POTENTIAL");
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const sectionY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
+  const shuffle = useCallback(() => {
+    const index = Math.floor(Math.random() * texts.length);
+    setText(texts[index]);
+  }, []); // Include dependency on array length
+
+  useEffect(() => {
+    const intervalID = setInterval(shuffle, 2000); // Adjust interval as needed
+    return () => clearInterval(intervalID);
+  }, [shuffle]); // Include dependency on shuffle function
+
+  return (
+    <div
+      ref={ref}
+      className="min-h-screen flex items-center justify-center bg-transparent w-full"
+    >
+      <video
+        className="object-cover h-full w-full fixed -z-10"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/background/hero/background.mp4" />
+      </video>
+      <motion.div
+        style={{ y: sectionY }}
+        className=" h-full flex flex-row justify-between w-[95%] -z-10"
+      >
+        <div className="w-2/3">
+          <h1 className="flex flex-col gap-5 text-6xl md:text-8xl text-secondary font-neueMedium">
+            we help brand grow to its fullest{" "}
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              key={Math.random()}
+              transition={{ duration: 1 }}
+              className="font-neueLight italic"
+              style={{ fontWeight: 1 }}
+            >
+              {text}
+            </motion.span>
+          </h1>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,89 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#1926FF",
+        },
+        secondary: {
+          DEFAULT: "#F9F871",
+          foreground: "#F9F7BB",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "#FF0074",
+        },
+        black: {
+          DEFAULT: "#00072d",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        anderson: "var(--font-anderson)",
+        apple: "var(--font-apple)",
+        moche: "var(--font-moche)",
+        mocheBold: "var(--font-moche-bold)",
+        neueMedium: "var(--font-neueMedium)",
+        neueLight: "var(--font-neueLight)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
