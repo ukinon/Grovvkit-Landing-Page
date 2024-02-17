a8894b8ca190b8ca17f5022f0f6b2d94cdcb5ff9
Arfiano Jordhy <arfianoj@gmail.com>
2024-02-17 20:27:46 +0700

    style:change initial background to zinc 950

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="snap-y snap-mandatory scroll-smooth ">
+    <html lang="en" className="snap-y snap-mandatory scroll-smooth bg-zinc-950">
       <body
         className={`${anderson.variable} ${appleGaramond.variable} ${moche.variable} ${mocheBold.variable} ${neueMedium.variable} ${neueLight.variable}`}
       >
